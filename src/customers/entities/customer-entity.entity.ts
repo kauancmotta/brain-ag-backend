89afc297ea7099c833ef0e01e1bdc0e988250fe9
wifzw <wifzg0d@gmail.com>
2026-08-18 10:31:38 -0300

    fix: add entity relationship to customer

--- src/customers/entities/customer-entity.entity.ts
+++ src/customers/entities/customer-entity.entity.ts
@@ -1,15 +1,16 @@
 import {
   CreateDateColumn,
   DeleteDateColumn,
-  Entity,
+  Entity as EntityORM,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Customer } from './customer.entity';
+import { Entity } from 'src/entities/entities/entity.entity';
 
-@Entity('customer_entities')
+@EntityORM('customer_entities')
 export class CustomerEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
@@ -17,6 +18,13 @@ export class CustomerEntity {
   @ManyToOne(() => Customer, (customer) => customer.customerEntities)
   @JoinColumn({ name: 'customer_id' })
   customer!: Customer;
+
+  @ManyToOne(() => Entity, (entity) => entity.customerEntities)
+  @JoinColumn({
+    name: 'entity_id',
+    foreignKeyConstraintName: 'fk_customers_entity_id',
+  })
+  entity!: Entity;
 
   @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt!: Date;
